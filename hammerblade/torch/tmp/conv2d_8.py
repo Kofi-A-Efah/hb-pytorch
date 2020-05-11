95431baed953a23e16126e7ed937e1e23054251d
Bandhav Veluri <bandhav.veluri00@gmail.com>
2020-05-11 14:29:27 -0700

    assertions

--- hammerblade/torch/tmp/conv2d_8.py
+++ hammerblade/torch/tmp/conv2d_8.py
@@ -3,7 +3,10 @@ import torch.nn.functional as F
 
 torch.manual_seed(42)
 
-inputs = torch.rand(1, 1, 8, 8).hammerblade()
-weights = torch.rand(1, 1, 3, 3).hammerblade()
+inputs = torch.rand(1, 1, 8, 8)
+weights = torch.rand(1, 1, 3, 3)
 
-print(F.conv2d(inputs, weights))
+out = F.conv2d(inputs, weights)
+out_hb = F.conv2d(inputs.hammerblade(), weights.hammerblade())
+
+assert torch.allclose(out, out_hb.cpu())
--- hammerblade/torch/tmp/mm_8.py
+++ hammerblade/torch/tmp/mm_8.py
@@ -3,7 +3,10 @@ import torch.nn.functional as F
 
 torch.manual_seed(42)
 
-x = torch.rand(8, 8).hammerblade()
-y = torch.rand(8, 8).hammerblade()
+x = torch.rand(8, 8)
+y = torch.rand(8, 8)
 
-print(torch.mm(x, y))
+out = torch.mm(x, y)
+out_hb = torch.mm(x.hammerblade(), y.hammerblade())
+
+assert torch.allclose(out, out_hb.cpu())
